d3ab9b2037b179dc7e08ed8380ce932276978f4e
Andy <andrea.carrillom0406@gmail.com>
2025-08-18 21:18:44 -0600

    Agregar README, actualizar app.js

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let nombreAmigo;
+let mostrarAmigos;
+let amigoAleatorio;
+let amigoSorteado;
+let mostrarAmigoSorteado;
+
+function asignarTextoElemento(id, texto){
+    let idHTML = document.getElementById(id);
+    idHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+let nombreAmigo = document.getElementById("amigo").value;
+console.log(nombreAmigo);
+if (nombreAmigo == ""){
+    alert("Por favor inserte un nombre");
+} else if (listaAmigos.includes(nombreAmigo)) {
+    alert("Ingrese otro nombre, ese ya ha sido ingresado");
+} else {
+    listaAmigos.push(nombreAmigo);
+    mostrarAmigo();
+    console.log(listaAmigos);
+}
+limpiarCaja();
+return;
+}
+
+function mostrarAmigo(){
+
+    //mostrarAmigos = asignarTextoElemento("listaAmigos", "");
+    mostrarAmigos = document.getElementById("listaAmigos");
+    mostrarAmigos.innerHTML = "";
+
+    for ( let i of listaAmigos){
+        let li = document.createElement("li");
+        li.textContent = i;
+        mostrarAmigos.appendChild(li);
+    }
+    return;
+}
+
+function sortearAmigo(){
+     if (listaAmigos.length != 0) {
+        amigoAleatorio = Math.floor(Math.random()*listaAmigos.length);
+        amigoSorteado = listaAmigos[amigoAleatorio];
+        
+        mostrarAmigoSorteado = document.getElementById("resultado");
+        mostrarAmigoSorteado.innerHTML = "";
+        mostrarAmigoSorteado.innerHTML = `<li>${amigoSorteado}</li>`
+        listaAmigos.splice(amigoAleatorio, 1);
+
+     }else{
+        alert("No hay amigos para sortear");
+        condicionesIniciales();
+     }
+} 
+
+function condicionesIniciales(){
+    mostrarAmigos = document.getElementById("listaAmigos").innerHTML = "";
+    mostrarAmigoSorteado = document.getElementById("resultado").innerHTML = "";
+}
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = "";
+    return;
+}
